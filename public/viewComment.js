2138bd6f44b154bbe67fe29622da5d40cceaf5b0
Sergio <principiante.en.programar@gmail.com>
2020-08-31 12:48:35 -0300

    update and functions, and add the functions

--- public/viewComment.js
+++ public/viewComment.js
@@ -19,27 +19,31 @@ class ViewCommet extends React.Component {
     eachComment(pic) {
         var media;
         if (pic.extension === "video") {
-            media = <video src={pic.urlPhoto} controls height="300px" width="250px"/>
+            media = <video src={pic.urlPhoto} controls height="300px" width="250px" />
         }
         else if (pic.extension === "audio") {
-            media = <audio src={pic.urlPhoto} />
+            media = <audio src={pic.urlPhoto} height="300px" width="250px" />
         }
         else if (pic.extension === "image") {
-            media = <img src={pic.urlPhoto} />
+            media = <img src={pic.urlPhoto} height="300px" width="250px" />
         }
         var user = "";
-        if(pic.anonimus){
-            user="Anonymus";
+        if (pic.anonimus) {
+            user = "Anonymus";
         }
-        else{
-            user=pic.user;
+        else {
+            user = pic.user;
         }
         return (
-            <div style={{"margin-top":"15px"}}>
+            <div style={{ "margin-top": "15px" }}>
                 <div>{user}</div>
                 <div>{pic.comment}</div>
                 <div>{media}</div>
                 <div>{pic.datePost}</div>
+                <div>
+                    <button value={pic._id}>Eliminar</button>
+                    <button value={pic._id}>Editar</button>
+                </div>
             </div>
         );
     }
